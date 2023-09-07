8e04d4436ff005cceec65d53840e44c1e6bedc0c
Damian Sire <damiansire@hotmail.com>
2023-09-07 11:25:26 -0300

    feat: export getByTag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const loadJSONFile = require('./libs/load-json-file');
-
+const { getByTag } = require('../src/libs/gets');
 
 module.exports = {
     sum: function (a, b) {
         return a + b;
     },
-    loadJSONFile
+    loadJSONFile,
+    getByTag
+
 }
